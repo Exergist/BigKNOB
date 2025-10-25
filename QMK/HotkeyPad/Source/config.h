4e9b2c104defb1e1ba6127270bca1a9c44cc2c51
Exergist <exergist.me@gmail.com>
2025-10-24 21:53:46 -0500

    updates

--- QMK/HotkeyPad/Source/config.h
+++ QMK/HotkeyPad/Source/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-/* USB Device descriptor parameter */
+// USB Device descriptor parameters
 #define VENDOR_ID       0xCEEB
 #define PRODUCT_ID      0x0007
 #define DEVICE_VER      0x0001
